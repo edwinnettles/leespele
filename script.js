eb40115b75d13a59a82bd1e8b567c1abcb3f41f6
Edwin Nettles <NateDzn@gmail.com>
2024-01-01 23:30:45 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 alert("this is a git test");
+
+const bestsellersSlider = new Swiper(".swiper.best-sellers", {
+  // Optional parameters
+  loop: true,
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  
+  //Navigation arrows
+  navigation: {
+  	nextEl: ".best-sellers-btn-next",
+    prevEl: ".best-sellers-btn-prev"
+  }
+});
